107667cd12d625890b0ecef4d8590932f378b973
ATIKSH09 <atikshgupta6373@gmail.com>
2025-02-16 02:49:05 +0530

    done routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Login from "./Pages/Login";
+import Landing from "./Pages/Landing";
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Landing />} />
           <Route path="/chat" element={<Sidebar />} />
           <Route path="/login" element={<Login />} />
         </Routes>
