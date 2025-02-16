6e3249275e0c7789558e1b363c390ec067d22fd7
harrydadev <harrydadev@gmail.com>
2025-02-16 08:41:08 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import Landing from "./Pages/Landing";
 import SpecificChat from "../components/SpecificChat";
 import NotFound from "./Pages/NotFound";
+import Chat from "../components/Chat";
 
 function App() {
   const queryClient = new QueryClient();
@@ -16,6 +17,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<Landing />} />
+            <Route path="/chat" element={<Sidebar />} />
             <Route path="/newchat" element={<Sidebar />} />
             <Route path="/chat/:id" element={<SpecificChat />} />
             <Route path="/login" element={<Login />} />
